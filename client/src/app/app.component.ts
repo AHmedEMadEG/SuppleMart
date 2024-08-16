c48793e5e19f8af9a072e3c4a1c566eb726b8444
ahmedabozied1020 <ahmedabozaied24@gmail.com>
2024-08-16 18:24:30 +0200

    solved conflict in app.html

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,12 +1,10 @@
-import { SalesCardsComponent } from './components/layout/sales-cards/sales-cards.component';
-import { ReviewRealCustomersComponent } from './components/layout/review-real-customers/review-real-customers.component';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, SalesCardsComponent, ReviewRealCustomersComponent],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
