72aec742197e3704b05278c99201c80c059042e3
ahmedabozied1020 <ahmedabozaied24@gmail.com>
2024-08-16 18:14:45 +0200

    add card components folder

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,13 +1,14 @@
+import { SalesCardsComponent } from './components/layout/sales-cards/sales-cards.component';
+import { ReviewRealCustomersComponent } from './components/layout/review-real-customers/review-real-customers.component';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { FilledButtonComponent } from './components/buttons/filled-button/filled-button.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, FilledButtonComponent],
+  imports: [RouterOutlet, SalesCardsComponent, ReviewRealCustomersComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'app';
