fd81d551818ba0e5cd45ec2af004d95c5061e15b
ahmedabozied1020 <ahmedabozaied24@gmail.com>
2024-08-25 19:50:05 +0200

    add proudct cart in child component

--- client/src/app/pages/cart/cart/cart.component.ts
+++ client/src/app/pages/cart/cart/cart.component.ts
@@ -1,3 +1,4 @@
+import { CartProductComponent } from './../../../components/cart-product/cart-product/cart-product.component';
 import { FooterComponent } from './../../../layouts/footer/footer.component';
 import { Component } from '@angular/core';
 import { NavbarComponent } from '../../../layouts/navbar/navbar.component';
@@ -5,7 +6,7 @@ import { NavbarComponent } from '../../../layouts/navbar/navbar.component';
 @Component({
   selector: 'app-cart',
   standalone: true,
-  imports: [NavbarComponent, FooterComponent],
+  imports: [NavbarComponent, FooterComponent, CartProductComponent],
   templateUrl: './cart.component.html',
   styleUrl: './cart.component.css',
 })
